f68a6934e755ea179ef938e5cac7c30ee378a50f
gsipin <117236228+gsipin@users.noreply.github.com>
2026-02-27 18:24:17 -0800

    Add files via upload

---
+++ review/masters/e1e0d7b6-308c-4145-9582-1dec36fc8b62/events-1772243127246.js
@@ -0,0 +1,142 @@
+jQuery("#simulation")
+  .on("click", ".m-e1e0d7b6-308c-4145-9582-1dec36fc8b62 .click,.m-e1e0d7b6-308c-4145-9582-1dec36fc8b62.click", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(jimUtil.isAlternateModeActive()) return;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    event.data = data;
+    jFirer = jEvent.getEventFirer();
+    if(jFirer.is("#Paragraph_2")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/3ddafdf6-a9e9-4dd8-b046-edb25ef5d8ed"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#Paragraph_4")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/968fd3ef-e3ec-4ce1-a3c5-daafc4d74c1a"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#Paragraph_6")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/d12245cc-1680-458d-89dd-4f0d7fb22724"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#Text_1")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/d12245cc-1680-458d-89dd-4f0d7fb22724"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#Text_2")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/6a369cc9-a9e6-48be-8c9b-e8632a0a180c"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#Text_3")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/a65db0f1-64f7-4df4-80b5-ec786f07be5f"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      jEvent.launchCases(cases);
+    }
+  });
