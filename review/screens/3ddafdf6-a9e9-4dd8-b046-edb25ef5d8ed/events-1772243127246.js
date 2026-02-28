eb1c329486b51c7a252e722b508958a890ce84cc
gsipin <117236228+gsipin@users.noreply.github.com>
2026-02-27 18:25:29 -0800

    Add files via upload

---
+++ review/screens/3ddafdf6-a9e9-4dd8-b046-edb25ef5d8ed/events-1772243127246.js
@@ -0,0 +1,537 @@
+jQuery("#simulation")
+  .on("click", ".s-3ddafdf6-a9e9-4dd8-b046-edb25ef5d8ed .click", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(jimUtil.isAlternateModeActive()) return;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    event.data = data;
+    jFirer = jEvent.getEventFirer();
+    if(jFirer.is("#s-Path_6")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimScrollTo",
+                  "parameter": {
+                    "target": [ "#s-Text_20" ],
+                    "axis": "scrollxy",
+                    "effect": {
+                      "type": "none",
+                      "easing": "linear",
+                      "duration": 500
+                    }
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#mi-a56cf1ce-Text_4")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/3ddafdf6-a9e9-4dd8-b046-edb25ef5d8ed"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#mi-a56cf1ce-Text_5")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/968fd3ef-e3ec-4ce1-a3c5-daafc4d74c1a"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#mi-a56cf1ce-Text_6")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/d12245cc-1680-458d-89dd-4f0d7fb22724"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#mi-a56cf1ce-Text_7")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/d12245cc-1680-458d-89dd-4f0d7fb22724"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#mi-a56cf1ce-Text_8")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/6a369cc9-a9e6-48be-8c9b-e8632a0a180c"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#mi-a56cf1ce-Text_9")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/a65db0f1-64f7-4df4-80b5-ec786f07be5f"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#mi-73cd8483-Image_2")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/a65db0f1-64f7-4df4-80b5-ec786f07be5f"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#mi-73cd8483-Button_1")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/a65db0f1-64f7-4df4-80b5-ec786f07be5f"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#mi-73cd8483-Text_2")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/a65db0f1-64f7-4df4-80b5-ec786f07be5f"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#mi-73cd8483-Text_3")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/a65db0f1-64f7-4df4-80b5-ec786f07be5f"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      jEvent.launchCases(cases);
+    }
+  })
+  .on("mouseenter dragenter", ".s-3ddafdf6-a9e9-4dd8-b046-edb25ef5d8ed .mouseenter", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(jimUtil.isAlternateModeActive()) return;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    event.data = data;
+    jFirer = jEvent.getDirectEventFirer(this);
+    if(jFirer.is("#s-Button_7") && jFirer.has(event.relatedTarget).length === 0) {
+      event.backupState = true;
+      event.target = jFirer;
+      window.overTargets.push(jFirer.get(0));
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimChangeCursor",
+                  "parameter": {
+                    "type": "pointer"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Button_8") && jFirer.has(event.relatedTarget).length === 0) {
+      event.backupState = true;
+      event.target = jFirer;
+      window.overTargets.push(jFirer.get(0));
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimChangeCursor",
+                  "parameter": {
+                    "type": "pointer"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Button_9") && jFirer.has(event.relatedTarget).length === 0) {
+      event.backupState = true;
+      event.target = jFirer;
+      window.overTargets.push(jFirer.get(0));
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimChangeCursor",
+                  "parameter": {
+                    "type": "pointer"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Button_2") && jFirer.has(event.relatedTarget).length === 0) {
+      event.backupState = true;
+      event.target = jFirer;
+      window.overTargets.push(jFirer.get(0));
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimChangeCursor",
+                  "parameter": {
+                    "type": "pointer"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Button_51") && jFirer.has(event.relatedTarget).length === 0) {
+      event.backupState = true;
+      event.target = jFirer;
+      window.overTargets.push(jFirer.get(0));
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimChangeCursor",
+                  "parameter": {
+                    "type": "pointer"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Button_49") && jFirer.has(event.relatedTarget).length === 0) {
+      event.backupState = true;
+      event.target = jFirer;
+      window.overTargets.push(jFirer.get(0));
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimChangeCursor",
+                  "parameter": {
+                    "type": "pointer"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Path_6") && jFirer.has(event.relatedTarget).length === 0) {
+      event.backupState = true;
+      event.target = jFirer;
+      window.overTargets.push(jFirer.get(0));
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimChangeCursor",
+                  "parameter": {
+                    "type": "auto"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Button_40") && jFirer.has(event.relatedTarget).length === 0) {
+      event.backupState = true;
+      event.target = jFirer;
+      window.overTargets.push(jFirer.get(0));
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimChangeCursor",
+                  "parameter": {
+                    "type": "pointer"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Button_39") && jFirer.has(event.relatedTarget).length === 0) {
+      event.backupState = true;
+      event.target = jFirer;
+      window.overTargets.push(jFirer.get(0));
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimChangeCursor",
+                  "parameter": {
+                    "type": "pointer"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      jEvent.launchCases(cases);
+    }
+  })
+  .on("mouseleave dragleave", ".s-3ddafdf6-a9e9-4dd8-b046-edb25ef5d8ed .mouseleave", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(jimUtil.isAlternateModeActive()) return;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    event.data = data;
+    jFirer = jEvent.getDirectEventFirer(this);
+    if(jFirer.is("#s-Button_7")) {
+      if (window.overTargets.indexOf(jFirer.get(0)) !== -1)
+      	window.overTargets.splice(window.overTargets.indexOf(jFirer.get(0)), 1);
+      jEvent.undoCases(jFirer);
+    } else if(jFirer.is("#s-Button_8")) {
+      if (window.overTargets.indexOf(jFirer.get(0)) !== -1)
+      	window.overTargets.splice(window.overTargets.indexOf(jFirer.get(0)), 1);
+      jEvent.undoCases(jFirer);
+    } else if(jFirer.is("#s-Button_9")) {
+      if (window.overTargets.indexOf(jFirer.get(0)) !== -1)
+      	window.overTargets.splice(window.overTargets.indexOf(jFirer.get(0)), 1);
+      jEvent.undoCases(jFirer);
+    } else if(jFirer.is("#s-Button_2")) {
+      if (window.overTargets.indexOf(jFirer.get(0)) !== -1)
+      	window.overTargets.splice(window.overTargets.indexOf(jFirer.get(0)), 1);
+      jEvent.undoCases(jFirer);
+    } else if(jFirer.is("#s-Button_51")) {
+      if (window.overTargets.indexOf(jFirer.get(0)) !== -1)
+      	window.overTargets.splice(window.overTargets.indexOf(jFirer.get(0)), 1);
+      jEvent.undoCases(jFirer);
+    } else if(jFirer.is("#s-Button_49")) {
+      if (window.overTargets.indexOf(jFirer.get(0)) !== -1)
+      	window.overTargets.splice(window.overTargets.indexOf(jFirer.get(0)), 1);
+      jEvent.undoCases(jFirer);
+    } else if(jFirer.is("#s-Path_6")) {
+      if (window.overTargets.indexOf(jFirer.get(0)) !== -1)
+      	window.overTargets.splice(window.overTargets.indexOf(jFirer.get(0)), 1);
+      jEvent.undoCases(jFirer);
+    } else if(jFirer.is("#s-Button_40")) {
+      if (window.overTargets.indexOf(jFirer.get(0)) !== -1)
+      	window.overTargets.splice(window.overTargets.indexOf(jFirer.get(0)), 1);
+      jEvent.undoCases(jFirer);
+    } else if(jFirer.is("#s-Button_39")) {
+      if (window.overTargets.indexOf(jFirer.get(0)) !== -1)
+      	window.overTargets.splice(window.overTargets.indexOf(jFirer.get(0)), 1);
+      jEvent.undoCases(jFirer);
+    }
+  });
