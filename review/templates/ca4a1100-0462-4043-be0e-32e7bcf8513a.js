761d2b61e845aa15fdbb71d1c129fc90a52d2b73
gsipin <117236228+gsipin@users.noreply.github.com>
2026-02-27 18:28:07 -0800

    Add files via upload

---
+++ review/templates/ca4a1100-0462-4043-be0e-32e7bcf8513a.js
@@ -0,0 +1,14 @@
+var content='<div class="ui-page " deviceName="web" deviceType="desktop" deviceWidth="1280" deviceHeight="800">\
+    <div id="t-ca4a1100-0462-4043-be0e-32e7bcf8513a" class="template growth-both devWeb canvas firer commentable non-processed" alignment="left" name="Template 2"width="1280" height="800">\
+    <div id="backgroundBox"><div class="colorLayer"></div><div class="imageLayer"></div></div>\
+    <div id="alignmentBox">\
+      <link type="text/css" rel="stylesheet" href="./review/templates/ca4a1100-0462-4043-be0e-32e7bcf8513a/style-1772243127246.css" />\
+      <div class="freeLayout">\
+      </div>\
+\
+      </div>\
+      <div id="loadMark"></div>\
+    </div>  \
+</div>\
+';
+document.getElementById("chromeTransfer").innerHTML = content;
