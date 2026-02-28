97efdc94573017c774bcae16d8d8d0026eeb9a5f
gsipin <117236228+gsipin@users.noreply.github.com>
2026-02-27 18:24:50 -0800

    Add files via upload

---
+++ review/scenarios/function-jim-links-1772243127246.js
@@ -0,0 +1,207 @@
+(function(window, undefined) {
+
+  var jimLinks = {
+    "3ddafdf6-a9e9-4dd8-b046-edb25ef5d8ed" : {
+      "Text_4" : [
+        "3ddafdf6-a9e9-4dd8-b046-edb25ef5d8ed"
+      ],
+      "Text_5" : [
+        "968fd3ef-e3ec-4ce1-a3c5-daafc4d74c1a"
+      ],
+      "Text_6" : [
+        "d12245cc-1680-458d-89dd-4f0d7fb22724"
+      ],
+      "Text_7" : [
+        "d12245cc-1680-458d-89dd-4f0d7fb22724"
+      ],
+      "Text_8" : [
+        "6a369cc9-a9e6-48be-8c9b-e8632a0a180c"
+      ],
+      "Text_9" : [
+        "a65db0f1-64f7-4df4-80b5-ec786f07be5f"
+      ],
+      "Image_2" : [
+        "a65db0f1-64f7-4df4-80b5-ec786f07be5f"
+      ],
+      "Button_1" : [
+        "a65db0f1-64f7-4df4-80b5-ec786f07be5f"
+      ],
+      "Text_2" : [
+        "a65db0f1-64f7-4df4-80b5-ec786f07be5f"
+      ],
+      "Text_3" : [
+        "a65db0f1-64f7-4df4-80b5-ec786f07be5f"
+      ]
+    },
+    "968fd3ef-e3ec-4ce1-a3c5-daafc4d74c1a" : {
+      "Image_2" : [
+        "a65db0f1-64f7-4df4-80b5-ec786f07be5f"
+      ],
+      "Button_6" : [
+        "a65db0f1-64f7-4df4-80b5-ec786f07be5f"
+      ],
+      "Text_3" : [
+        "a65db0f1-64f7-4df4-80b5-ec786f07be5f"
+      ],
+      "Text_4" : [
+        "a65db0f1-64f7-4df4-80b5-ec786f07be5f"
+      ],
+      "Text_9" : [
+        "3ddafdf6-a9e9-4dd8-b046-edb25ef5d8ed"
+      ],
+      "Text_14" : [
+        "968fd3ef-e3ec-4ce1-a3c5-daafc4d74c1a"
+      ],
+      "Text_15" : [
+        "d12245cc-1680-458d-89dd-4f0d7fb22724"
+      ],
+      "Text_16" : [
+        "d12245cc-1680-458d-89dd-4f0d7fb22724"
+      ],
+      "Text_17" : [
+        "6a369cc9-a9e6-48be-8c9b-e8632a0a180c"
+      ],
+      "Text_18" : [
+        "a65db0f1-64f7-4df4-80b5-ec786f07be5f"
+      ]
+    },
+    "a65db0f1-64f7-4df4-80b5-ec786f07be5f" : {
+      "Text_1" : [
+        "3ddafdf6-a9e9-4dd8-b046-edb25ef5d8ed"
+      ],
+      "Text_2" : [
+        "968fd3ef-e3ec-4ce1-a3c5-daafc4d74c1a"
+      ],
+      "Text_3" : [
+        "d12245cc-1680-458d-89dd-4f0d7fb22724"
+      ],
+      "Text_9" : [
+        "d12245cc-1680-458d-89dd-4f0d7fb22724"
+      ],
+      "Text_14" : [
+        "6a369cc9-a9e6-48be-8c9b-e8632a0a180c"
+      ],
+      "Text_15" : [
+        "a65db0f1-64f7-4df4-80b5-ec786f07be5f"
+      ]
+    },
+    "6a369cc9-a9e6-48be-8c9b-e8632a0a180c" : {
+      "Image_6" : [
+        "a65db0f1-64f7-4df4-80b5-ec786f07be5f"
+      ],
+      "Button_5" : [
+        "a65db0f1-64f7-4df4-80b5-ec786f07be5f"
+      ],
+      "Text_17" : [
+        "a65db0f1-64f7-4df4-80b5-ec786f07be5f"
+      ],
+      "Text_18" : [
+        "a65db0f1-64f7-4df4-80b5-ec786f07be5f"
+      ],
+      "Text_1" : [
+        "3ddafdf6-a9e9-4dd8-b046-edb25ef5d8ed"
+      ],
+      "Text_2" : [
+        "968fd3ef-e3ec-4ce1-a3c5-daafc4d74c1a"
+      ],
+      "Text_3" : [
+        "d12245cc-1680-458d-89dd-4f0d7fb22724"
+      ],
+      "Text_9" : [
+        "d12245cc-1680-458d-89dd-4f0d7fb22724"
+      ],
+      "Text_14" : [
+        "6a369cc9-a9e6-48be-8c9b-e8632a0a180c"
+      ],
+      "Text_15" : [
+        "a65db0f1-64f7-4df4-80b5-ec786f07be5f"
+      ]
+    },
+    "d12245cc-1680-458d-89dd-4f0d7fb22724" : {
+      "Image_6" : [
+        "a65db0f1-64f7-4df4-80b5-ec786f07be5f"
+      ],
+      "Button_4" : [
+        "a65db0f1-64f7-4df4-80b5-ec786f07be5f"
+      ],
+      "Paragraph_31" : [
+        "a65db0f1-64f7-4df4-80b5-ec786f07be5f"
+      ],
+      "Paragraph_3" : [
+        "a65db0f1-64f7-4df4-80b5-ec786f07be5f"
+      ],
+      "Path_5" : [
+        "6a369cc9-a9e6-48be-8c9b-e8632a0a180c"
+      ],
+      "Text_4" : [
+        "6a369cc9-a9e6-48be-8c9b-e8632a0a180c"
+      ],
+      "Path_135" : [
+        "3ddafdf6-a9e9-4dd8-b046-edb25ef5d8ed"
+      ],
+      "Text_10" : [
+        "3ddafdf6-a9e9-4dd8-b046-edb25ef5d8ed"
+      ],
+      "Path_3" : [
+        "968fd3ef-e3ec-4ce1-a3c5-daafc4d74c1a"
+      ],
+      "Text_6" : [
+        "968fd3ef-e3ec-4ce1-a3c5-daafc4d74c1a"
+      ],
+      "Paragraph_2" : [
+        "3ddafdf6-a9e9-4dd8-b046-edb25ef5d8ed"
+      ],
+      "Paragraph_4" : [
+        "968fd3ef-e3ec-4ce1-a3c5-daafc4d74c1a"
+      ],
+      "Paragraph_6" : [
+        "d12245cc-1680-458d-89dd-4f0d7fb22724"
+      ],
+      "Text_1" : [
+        "d12245cc-1680-458d-89dd-4f0d7fb22724"
+      ],
+      "Text_2" : [
+        "6a369cc9-a9e6-48be-8c9b-e8632a0a180c"
+      ],
+      "Text_3" : [
+        "a65db0f1-64f7-4df4-80b5-ec786f07be5f"
+      ]
+    },
+    "583fe023-f6c3-4f4d-b7e9-02eb84cca072" : {
+      "Image_6" : [
+        "a65db0f1-64f7-4df4-80b5-ec786f07be5f"
+      ],
+      "Button_4" : [
+        "a65db0f1-64f7-4df4-80b5-ec786f07be5f"
+      ],
+      "Paragraph_31" : [
+        "a65db0f1-64f7-4df4-80b5-ec786f07be5f"
+      ],
+      "Paragraph_3" : [
+        "a65db0f1-64f7-4df4-80b5-ec786f07be5f"
+      ]
+    },
+    "e1e0d7b6-308c-4145-9582-1dec36fc8b62" : {
+      "Paragraph_2" : [
+        "3ddafdf6-a9e9-4dd8-b046-edb25ef5d8ed"
+      ],
+      "Paragraph_4" : [
+        "968fd3ef-e3ec-4ce1-a3c5-daafc4d74c1a"
+      ],
+      "Paragraph_6" : [
+        "d12245cc-1680-458d-89dd-4f0d7fb22724"
+      ],
+      "Text_1" : [
+        "d12245cc-1680-458d-89dd-4f0d7fb22724"
+      ],
+      "Text_2" : [
+        "6a369cc9-a9e6-48be-8c9b-e8632a0a180c"
+      ],
+      "Text_3" : [
+        "a65db0f1-64f7-4df4-80b5-ec786f07be5f"
+      ]
+    }    
+  }
+
+  window.jimLinks = jimLinks;
+})(window);
