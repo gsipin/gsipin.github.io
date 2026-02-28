15bbe90560ac825499625df5f529f670a07bd2a6
gsipin <117236228+gsipin@users.noreply.github.com>
2026-02-27 18:23:43 -0800

    Add files via upload

---
+++ review/masters/583fe023-f6c3-4f4d-b7e9-02eb84cca072.js
@@ -0,0 +1,71 @@
+var content='<div class="ui-page ui-component" deviceName="web" deviceType="desktop" deviceWidth="1280" deviceHeight="418">\
+    <div id="m-583fe023-f6c3-4f4d-b7e9-02eb84cca072" class="master growth-both devWeb canvas firer ie-background commentable non-processed" alignment="left" name="CTA "width="1280" height="418">\
+    <div id="backgroundBox"><div class="colorLayer"></div><div class="imageLayer"></div></div>\
+    <div id="alignmentBox">\
+      <link type="text/css" rel="stylesheet" href="./review/masters/583fe023-f6c3-4f4d-b7e9-02eb84cca072/style-1772243127246.css" />\
+      <link type="text/css" rel="stylesheet" href="./review/masters/583fe023-f6c3-4f4d-b7e9-02eb84cca072/fonts-1772243127246.css" />\
+      <div class="freeLayout">\
+      <div id="Group_11" class="group firer ie-background commentable non-processed" customid="CTA " datasizewidth="1280.00px" datasizeheight="492.55px" >\
+        <div id="Image_6" class="image firer click ie-background commentable non-processed" customid="Background"   datasizewidth="1280.97px" datasizeheight="418.05px" dataX="0.00" dataY="0.00"   alt="image">\
+          <div class="borderLayer">\
+          	<div class="imageViewport">\
+          		<img src="./images/b225e896-6bd9-438c-90fc-1345b37c7f2b.jpg" />\
+          	</div>\
+          </div>\
+        </div>\
+\
+        <div id="Button_4" class="button multiline manualfit firer click commentable non-processed" customid="Button"   datasizewidth="169.64px" datasizeheight="37.00px" dataX="66.00" dataY="190.52" >\
+          <div class="backgroundLayer">\
+            <div class="colorLayer"></div>\
+            <div class="imageLayer"></div>\
+          </div>\
+          <div class="borderLayer">\
+            <div class="paddingLayer">\
+              <div class="content">\
+                <div class="valign">\
+                  <span id="rtr-Button_4_0">Visit our FAQ</span>\
+                </div>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+        <div id="Paragraph_31" class="richtext autofit firer click ie-background commentable non-processed" customid="Subtitle"   datasizewidth="217.20px" datasizeheight="32.00px" dataX="69.40" dataY="137.64" >\
+          <div class="backgroundLayer">\
+            <div class="colorLayer"></div>\
+            <div class="imageLayer"></div>\
+          </div>\
+          <div class="borderLayer">\
+            <div class="paddingLayer">\
+              <div class="content">\
+                <div class="valign">\
+                  <span id="rtr-Paragraph_31_0">We have answers!</span>\
+                </div>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+        <div id="Paragraph_3" class="richtext autofit firer click ie-background commentable non-processed" customid="Title"   datasizewidth="477.24px" datasizeheight="44.00px" dataX="66.00" dataY="81.64" >\
+          <div class="backgroundLayer">\
+            <div class="colorLayer"></div>\
+            <div class="imageLayer"></div>\
+          </div>\
+          <div class="borderLayer">\
+            <div class="paddingLayer">\
+              <div class="content">\
+                <div class="valign">\
+                  <span id="rtr-Paragraph_3_0">Have more questions? </span>\
+                </div>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+      </div>\
+\
+      </div>\
+\
+      </div>\
+      <div id="loadMark"></div>\
+    </div>  \
+</div>\
+';
+document.getElementById("chromeTransfer").innerHTML = content;
---
+++ review/masters/e1e0d7b6-308c-4145-9582-1dec36fc8b62.js
@@ -0,0 +1,108 @@
+var content='<div class="ui-page ui-component" deviceName="web" deviceType="desktop" deviceWidth="924" deviceHeight="31">\
+    <div id="m-e1e0d7b6-308c-4145-9582-1dec36fc8b62" class="master growth-both devWeb canvas firer ie-background commentable non-processed" alignment="left" name="Menu"width="924" height="31">\
+    <div id="backgroundBox"><div class="colorLayer"></div><div class="imageLayer"></div></div>\
+    <div id="alignmentBox">\
+      <link type="text/css" rel="stylesheet" href="./review/masters/e1e0d7b6-308c-4145-9582-1dec36fc8b62/style-1772243127246.css" />\
+      <link type="text/css" rel="stylesheet" href="./review/masters/e1e0d7b6-308c-4145-9582-1dec36fc8b62/fonts-1772243127246.css" />\
+      <div class="freeLayout">\
+      <div id="Group_10" class="group firer ie-background commentable non-processed" customid="Menu" datasizewidth="924.98px" datasizeheight="31.00px" >\
+        <div id="Paragraph_2" class="richtext manualfit firer click ie-background commentable non-processed" customid="Activities"   datasizewidth="61.21px" datasizeheight="17.00px" dataX="669.42" dataY="3.09" >\
+          <div class="backgroundLayer">\
+            <div class="colorLayer"></div>\
+            <div class="imageLayer"></div>\
+          </div>\
+          <div class="borderLayer">\
+            <div class="paddingLayer">\
+              <div class="content">\
+                <div class="valign">\
+                  <span id="rtr-Paragraph_2_0">Activities</span>\
+                </div>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+        <div id="Paragraph_4" class="richtext autofit firer click ie-background commentable non-processed" customid="Lodging"   datasizewidth="51.67px" datasizeheight="16.00px" dataX="578.81" dataY="3.09" >\
+          <div class="backgroundLayer">\
+            <div class="colorLayer"></div>\
+            <div class="imageLayer"></div>\
+          </div>\
+          <div class="borderLayer">\
+            <div class="paddingLayer">\
+              <div class="content">\
+                <div class="valign">\
+                  <span id="rtr-Paragraph_4_0">Lodging</span>\
+                </div>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+        <div id="Paragraph_6" class="richtext autofit firer click ie-background commentable non-processed" customid="Home"   datasizewidth="39.72px" datasizeheight="17.00px" dataX="499.16" dataY="3.09" >\
+          <div class="backgroundLayer">\
+            <div class="colorLayer"></div>\
+            <div class="imageLayer"></div>\
+          </div>\
+          <div class="borderLayer">\
+            <div class="paddingLayer">\
+              <div class="content">\
+                <div class="valign">\
+                  <span id="rtr-Paragraph_6_0">Home</span>\
+                </div>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+        <div id="Text_1" class="richtext autofit firer click ie-background commentable non-processed" customid="DiscoverTaniti"   datasizewidth="209.55px" datasizeheight="31.00px" dataX="-0.00" dataY="-0.00" >\
+          <div class="backgroundLayer">\
+            <div class="colorLayer"></div>\
+            <div class="imageLayer"></div>\
+          </div>\
+          <div class="borderLayer">\
+            <div class="paddingLayer">\
+              <div class="content">\
+                <div class="valign">\
+                  <span id="rtr-Text_1_0">Discover</span><span id="rtr-Text_1_1">Taniti</span>\
+                </div>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+        <div id="Text_2" class="richtext manualfit firer click ie-background commentable non-processed" customid="Transportation"   datasizewidth="99.70px" datasizeheight="17.00px" dataX="763.25" dataY="3.09" >\
+          <div class="backgroundLayer">\
+            <div class="colorLayer"></div>\
+            <div class="imageLayer"></div>\
+          </div>\
+          <div class="borderLayer">\
+            <div class="paddingLayer">\
+              <div class="content">\
+                <div class="valign">\
+                  <span id="rtr-Text_2_0">Transportation</span>\
+                </div>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+        <div id="Text_3" class="richtext manualfit firer click ie-background commentable non-processed" customid="FAQ"   datasizewidth="34.10px" datasizeheight="17.00px" dataX="890.88" dataY="3.09" >\
+          <div class="backgroundLayer">\
+            <div class="colorLayer"></div>\
+            <div class="imageLayer"></div>\
+          </div>\
+          <div class="borderLayer">\
+            <div class="paddingLayer">\
+              <div class="content">\
+                <div class="valign">\
+                  <span id="rtr-Text_3_0">FAQ</span>\
+                </div>\
+              </div>\
+            </div>\
+          </div>\
+        </div>\
+      </div>\
+\
+      </div>\
+\
+      </div>\
+      <div id="loadMark"></div>\
+    </div>  \
+</div>\
+';
+document.getElementById("chromeTransfer").innerHTML = content;
