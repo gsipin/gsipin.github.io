a16e69c6d511ddf362dbd6b289d5dcba119b0430
gsipin <117236228+gsipin@users.noreply.github.com>
2026-02-27 18:24:00 -0800

    Add files via upload

---
+++ review/masters/583fe023-f6c3-4f4d-b7e9-02eb84cca072/events-1772243127246.js
@@ -0,0 +1,98 @@
+jQuery("#simulation")
+  .on("click", ".m-583fe023-f6c3-4f4d-b7e9-02eb84cca072 .click,.m-583fe023-f6c3-4f4d-b7e9-02eb84cca072.click", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(jimUtil.isAlternateModeActive()) return;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    event.data = data;
+    jFirer = jEvent.getEventFirer();
+    if(jFirer.is("#Image_6")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/a65db0f1-64f7-4df4-80b5-ec786f07be5f"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#Button_4")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/a65db0f1-64f7-4df4-80b5-ec786f07be5f"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#Paragraph_31")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/a65db0f1-64f7-4df4-80b5-ec786f07be5f"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#Paragraph_3")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/a65db0f1-64f7-4df4-80b5-ec786f07be5f"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      jEvent.launchCases(cases);
+    }
+  });
